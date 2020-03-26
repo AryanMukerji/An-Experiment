ddc05068c73298f22d3f9c0c2dd9b78abefad871
Aryan Mukerji <aryanmukerji@gmail.com>
2020-03-26 23:33:21 +0530

    Edit From Test Branch

--- Experiment.cpp
+++ Experiment.cpp
@@ -8,14 +8,16 @@ int main()
 {
     int number, i = 1, factorial = 1;
 
-    cout << "Enter a positive integer: ";
+    cout << "\n Enter a positive integer : ";
     cin >> number;
     
-    while ( i <= number) {
+    while ( i <= number ) 
+	{
         factorial *= i;      //factorial = factorial * i;
         ++i;
     }
 
-    cout<<"Factorial of "<< number <<" = "<< factorial;
+    cout<< "\n Factorial of " << number << " = " << factorial;
+	
     return 0;
 }
